82919d651e7430ea05cf03cbf35b3923d3c5f525
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-11-25 10:27:53 +0100

    fix test

--- test/test_novell_plugin.rb
+++ test/test_novell_plugin.rb
@@ -11,17 +11,16 @@ class NovellPlugin_test < Test::Unit::TestCase
       client.url
     end
 
-    assert_equal 'https://apibugzilla.novell.com/tr_xmlrpc.cgi', "#{client.api_url.scheme}://#{client.api_url.host}#{client.api_url.path}"
+    assert_equal 'https://apibugzilla.novell.com/xmlrpc.cgi', "#{client.api_url.scheme}://#{client.api_url.host}#{client.api_url.path}"
     assert_equal 'https://bugzilla.novell.com', "#{client.site_url.scheme}://#{client.site_url.host}#{client.site_url.path}"
-
   end
 
   def self.write_fake_oscrc(path)
     File.open(path, 'w') do |f|
       f.write(<<EOS)
 [https://api.opensuse.org]
-user= foo
-pass=bar
+user = foo
+pass = bar
 # fake osc file
 EOS
     end
@@ -30,13 +29,14 @@ EOS
   def test_oscrc_parsing
     Dir.mktmpdir do |tmp|
       fake_oscrc = File.join(tmp, 'oscrc')
-      write_fake_oscrc(fake_oscrc)
+      NovellPlugin_test.write_fake_oscrc(fake_oscrc)
       Bicho::Plugins::Novell.oscrc_path = fake_oscrc
       plugin = Bicho::Plugins::Novell.new
       credentials = Bicho::Plugins::Novell.oscrc_credentials
       assert_not_nil(credentials)
       assert(credentials.has_key?(:user))
       assert(credentials.has_key?(:password))
+      Bicho::Plugins::Novell.oscrc_path = nil
     end
   end
 
