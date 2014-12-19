1d7f359072703afc7c4da3899557e30c9a02b6c6
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-12-19 13:37:45 +0100

    a plugin that provides other well-known aliases

---
+++ lib/bicho/plugins/aliases.rb
@@ -0,0 +1,16 @@
+module Bicho
+  module Plugins
+
+    class Aliases
+      def transform_site_url_hook(url, _logger)
+        case url.to_s
+        when 'bko', 'kernel' then 'https://bugzilla.kernel.org'
+        when 'bgo', 'gnome' then 'https://bugzilla.gnome.org'
+        when 'kde' then 'https://bugzilla.kde.org'
+        else url
+        end
+      end
+    end
+
+  end
+end
