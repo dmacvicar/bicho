61702023d1a214ff89716c946f5c90199c12df42
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-06-20 09:38:01 +0200

    use assert_match and update bz version

--- test/test_version.rb
+++ test/test_version.rb
@@ -8,14 +8,15 @@ class Version_test < Test::Unit::TestCase
     Bicho.client = Bicho::Client.new('https://bugzilla.gnome.org')
 
     ret = Bicho.client.version
-    assert ret =~ /3.4/ # https://bugzilla.gnome.org/ is at 3.4.13 as of Jan/2015
+    # https://bugzilla.gnome.org/ is at 4.4.12 as of Jun/2016
+    assert_match(/4.4/, ret)
   end
 
   def test_version_suse
     Bicho.client = Bicho::Client.new('https://bugzilla.suse.com')
 
     ret = Bicho.client.version
-    assert ret =~ /4.4/ # https://bugzilla.suse.com is at 4.4.6 as of Jan/2015
+    # https://bugzilla.suse.com is at 4.4.6 as of Jan/2015
+    assert_match(/4.4/, ret)
   end
-
 end
